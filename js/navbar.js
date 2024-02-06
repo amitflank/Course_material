2deae140bd562304ca103e93ed2a1e2b69ec4509
Amit <amitf82@gmail.com>
2024-02-06 13:26:55 -0700

    lots of file rearagement:

--- js/navbar.js
+++ js/navbar.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
   const navbarContainer = document.querySelector("#navbar-container");
 
-  fetch("../navbar.html")
+  fetch("../navbar/navbar.html")
     .then(response => response.text())
     .then(html => {
       navbarContainer.innerHTML = html;
--- js/web_dev.js
+++ js/web_dev.js
@@ -1,11 +1,11 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const navbarContainer = document.querySelector("#prog-container");
+    const navbarContainer = document.querySelector("#web-dev-container");
   
-    fetch("../navbar/programming.html")
+    fetch("../navbar/web_dev.html")
       .then(response => response.text())
       .then(html => {
         navbarContainer.innerHTML = html;
-        initializeDropdowns("prog-container");
+        initializeDropdowns("web-dev-container");
       })
       .catch(err => console.warn("Something went wrong with loading the navbar:", err));
   });
